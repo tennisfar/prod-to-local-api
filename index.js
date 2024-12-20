a7bf2c62c3d982dd78019628450e924f04fa5ea0
Michael L Petersen <ekmlpe@danskespil.dk>
2024-12-20 10:12:39 +0100

    :bus:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 1336;
 
 const corsOptions = {
   origin: 'https://web.develop.danskespil.dk',
